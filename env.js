7d8469986235a5451c347fb6a7fdd7a2dadb8f77
nekonoyun <51334430+nekonoyun@users.noreply.github.com>
2025-06-15 23:05:26 +0900

    アイコンの path を定数から取得するよう変更

---
+++ env.js
@@ -0,0 +1,8 @@
+const imgPath = 'https://pbs.twimg.com/profile_images'
+const imgId = '1934246235327868928/3uqfs3jU'
+
+const faviconImg = document.getElementById("faviconImg");
+const profileImg = document.getElementById("profileImg");
+
+faviconImg.href = imgPath + '/' + imgId + '_normal.jpg';
+profileImg.src = imgPath + '/' + imgId + '_400x400.jpg';
