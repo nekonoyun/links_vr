283351dbb99d447d3f98f8e311e88753da3c2531
nekonoyun <51334430+nekonoyun@users.noreply.github.com>
2025-07-02 23:11:25 +0900

    アイコンの更新を反映

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 const imgPath = 'https://pbs.twimg.com/profile_images'
-const imgId = '1934246235327868928/3uqfs3jU'
+const imgId = '1940383195616231424/-etCXOTy'
 
 window.addEventListener('DOMContentLoaded', () => {
   const faviconImg = document.getElementById("faviconImg");
