e53ee74b8efc6b35815f0fe7e176324a8eae9111
nekonoyun <51334430+nekonoyun@users.noreply.github.com>
2025-09-03 17:19:14 +0900

    Resonite の項目を追加, VRSNS 全般に適した表記へと変更

---
+++ copy.js
@@ -0,0 +1,23 @@
+var username = {}
+username['Resonite'] = 'Yuun'
+
+const copyBtnReso = document.getElementById('btn_resonite')
+copyBtnReso.href = 'javascript:void(0);'
+copyBtnReso.addEventListener('click', () => {
+  var tagType = 'Resonite'
+  copyToClipboard(tagType + ' のユーザー名', username[tagType])
+});
+
+function copyToClipboard (tagType, tagValue) {
+  if (navigator.clipboard) {
+    return navigator.clipboard.writeText(tagValue).then(function () {
+      alert(tagType + 'がクリップボードにコピーされました')
+      messageActive()
+    })
+  } else {
+    tagText.select()
+    document.execCommand('copy')
+    alert(tagType + 'がクリップボードにコピーされました')
+    messageActive()
+  }
+}
--- env.js
+++ env.js
@@ -2,9 +2,9 @@ const imgPath = 'https://pbs.twimg.com/profile_images'
 const imgId = '1940383195616231424/-etCXOTy'
 
 window.addEventListener('DOMContentLoaded', () => {
-  const faviconImg = document.getElementById("faviconImg");
-  const profileImg = document.getElementById("profileImg");
+  const faviconImg = document.getElementById('faviconImg')
+  const profileImg = document.getElementById('profileImg')
 
-  faviconImg.href = imgPath + '/' + imgId + '_normal.jpg';
-  profileImg.src = imgPath + '/' + imgId + '_400x400.jpg';
+  faviconImg.href = imgPath + '/' + imgId + '_normal.jpg'
+  profileImg.src = imgPath + '/' + imgId + '_400x400.jpg'
 })
