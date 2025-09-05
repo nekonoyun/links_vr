0c8ef02d943d1f5dde12438f5b8fbec1a3e0f761
nekonoyun <51334430+nekonoyun@users.noreply.github.com>
2025-09-05 19:53:06 +0900

    変数名のタイポを修正

--- env.js
+++ env.js
@@ -1,10 +1,10 @@
-const usrJd = 'Yuun'
+const usrId = 'Yuun'
 const usrName = 'ゆん'
 const imgPath = 'https://pbs.twimg.com/profile_images'
 const imgId = '1940383195616231424/-etCXOTy'
 
 window.addEventListener('DOMContentLoaded', () => {
-  document.title = usrName + ' (' + usrJd + ') | ' + document.title
+  document.title = usrName + ' (' + usrId + ') | ' + document.title
   const faviconImg = document.getElementById('faviconImg')
   const profileImg = document.getElementById('profileImg')
 
@@ -12,5 +12,5 @@ window.addEventListener('DOMContentLoaded', () => {
   profileImg.src = imgPath + '/' + imgId + '_400x400.jpg'
 
   const userVar = document.getElementById('userVar')
-  userVar.textContent = usrName + ' (' + usrJd + ')'
+  userVar.textContent = usrName + ' (' + usrId + ')'
 })
